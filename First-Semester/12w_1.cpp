f7416b7d85e33a48b97ce190643c5ad897b1a2cc
BaekRyang <79979444+BaekRyang@users.noreply.github.com>
2021-10-06 20:32:25 +0900

    Delete First-Semester directory

--- First-Semester/12w_1.cpp
+++
@@ -1,8 +0,0 @@
-////외부변수 : 외부에 이미 만들어진 변수를 사용할 때 사용한다.
-////전역변수만 extern 으로 사용가능하며 전역변수 더라도 정적변수는 사용할 수 없다.
-//extern int var1;
-////extern int var4;
-//
-//int increase() {
-//	return ++var1/*, ++var4*/;
-//}

e3e57dd31ebea35dfe76ca8e5914e127b654b90b
BaekRyang <graywolf147@gmail.com>
2021-10-07 15:36:09 +0900

    Mutex.cpp 설명 첨부

--- Network/Threading/Mutex.cpp
+++ Network/Threading/Mutex.cpp
@@ -10,16 +10,21 @@ int counter;
 HANDLE h_mutex; //CreateMutex에서 나온 HANDLE을 받을 변수 생성
 
 unsigned __stdcall func(LPVOID prm) {
+	/* Argment 처리 */
 	int start = *((int*)prm);//LPVOID*로 받은 데이터를 int*로 바꾼뒤 앞에 "*"를 붙여 값 추출
 	int end = *((int*)prm + 1);//그 다음 값을 받아오기 위해서는 포인터 +1 을 함.
+
+	//Argment로 받아온 Start부터 end까지 반복
 	for (int i = start; i < end; i++) {
+		//Key 받기
 		WaitForSingleObject(h_mutex, INFINITE);
 		//핸들값으로 받아온 h_mutex를 준다
-		//Auto Reset으로 인해 h_mutex는 non-signal로 초기화 됨
+		//Auto Reset으로 인해 h_mutex는 non-signal로 초기화 됨 ** ResetEvent()와 다르게 하드웨어적으로 보장 되어있음 **
 
 		counter++;
 
-		ReleaseMutex(h_mutex);
+		//Key 반납
+		ReleaseMutex(h_mutex); //만약 ReleaseMutex가 없더라도 자동으로 O/S에서 Signalled 상태로 만들어준다.
 		//h_mutex의 상태를 Signalled로 변환
 	}
 	return 0;
@@ -27,7 +32,7 @@ unsigned __stdcall func(LPVOID prm) {
 
 int main() {
 	HANDLE th[16];
-	int arg[] = { 0,100000 };
+	int arg[] = { 0,100 }; //func()을 할때 넘겨줄 Argment를 저장
 
 	//뮤텍스 커널 오브젝트를 생성
 	h_mutex = CreateMutex(NULL, false, NULL);//(보안값, Owner를 Create를 호출한 사람이 갖는가?, 이름) 핸들값 반환
@@ -37,18 +42,22 @@ int main() {
 	GetSystemInfo(&info);
 	cout << "Number of Proccessors is " << info.dwNumberOfProcessors << endl;
 
-	clock_t stime = clock();
+	clock_t stime = clock(); //시간 측정용
+
+	//Thread 생성
 	for (DWORD i = 0; i < info.dwNumberOfProcessors; i++) {
-		th[i] = (HANDLE)_beginthreadex(NULL, 0, &func, arg/*Arg를 주소값으로 넘겨줌*/, 0, NULL);
+		//HANDLE 배열 th에 Thread를 생성하여 handle을 넣어준다.
+		//Parameter로 [Security], [Buf사이즈], [함수주소], [Argment] [FLAG], [Thread 주소]를 사용한다. 
+
+		th[i] = (HANDLE)_beginthreadex(NULL, 0, &func, arg, 0, NULL);
 	}
 
 	WaitForMultipleObjects(info.dwNumberOfProcessors, th, true, INFINITE);
-	clock_t etime = clock();
-	//WaitForMultipleObjects(반복할 횟수, 핸들의 값을 모아둔 주소값, 전부 실행할 때 까지 대기?, TIMEOUT 시간)
 
-	//WaitForSingleObject(th, INFINITE);
+
+	clock_t etime = clock(); //시간 측정용
 
 	cout << "main : " << counter << endl
-		<< "Time : " << (double)((etime)-(stime)) / CLOCKS_PER_SEC << endl;
+		 << "Time : " << (double)((etime)-(stime)) / CLOCKS_PER_SEC << endl;
 	return 0;
 }
